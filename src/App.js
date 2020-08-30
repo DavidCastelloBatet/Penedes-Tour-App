c16c48ec84c37ae197b1a3c747acbbc14d23e946
David Castello <davidcastellobatetgmail.com>
2020-08-30 22:23:01 +0200

    CSS fitxa poblacio, crear estat , pasar dades Tour

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+
 import "./App.scss";
 
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <main>
+        <Navbar />
+        <TourList />
+      </main>
+    );
   }
 }
 
