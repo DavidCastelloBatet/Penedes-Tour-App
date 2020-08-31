097f54b1ef41cd81bc40b704effba19dabe39fb9
David Castello <davidcastellobatetgmail.com>
2020-08-31 23:22:23 +0200

    pujo buil final

---
+++ docs/precache-manifest.06be5cd70c43813a990828604ea7323c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130f37cd60c0905bc0bd205491ddda6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aeefd5f333199927262",
+    "url": "/static/css/main.b7444672.chunk.css"
+  },
+  {
+    "revision": "b0ceb776f3260f7abcfc",
+    "url": "/static/js/2.8d2ac844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d2ac844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aeefd5f333199927262",
+    "url": "/static/js/main.c241cdd5.chunk.js"
+  },
+  {
+    "revision": "0827a39c50e1f380d786",
+    "url": "/static/js/runtime-main.4d91bcc7.js"
+  }
+]);
