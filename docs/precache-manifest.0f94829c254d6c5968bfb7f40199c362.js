1f62212d0a7615e78199a7fe122bdf40aea8b004
David Castello <davidcastellobatetgmail.com>
2020-08-31 21:52:38 +0200

    preparacio pagina

---
+++ docs/precache-manifest.0f94829c254d6c5968bfb7f40199c362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111733ad57bb460a3c82eb49e830d657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee017ccac07d506e9bd",
+    "url": "/static/css/main.b7444672.chunk.css"
+  },
+  {
+    "revision": "94b9a7c5b0a05a8e08fe",
+    "url": "/static/js/2.33a9be3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33a9be3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee017ccac07d506e9bd",
+    "url": "/static/js/main.6412fe7b.chunk.js"
+  },
+  {
+    "revision": "0827a39c50e1f380d786",
+    "url": "/static/js/runtime-main.4d91bcc7.js"
+  },
+  {
+    "revision": "568d22cfc2009badb0fa14d4bbb52d78",
+    "url": "/static/media/logo.568d22cf.png"
+  }
+]);
